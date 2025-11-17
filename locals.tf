7ddecda581ffbc3ed33ff654a8d1dab2bee70009
DevOpsChandraTech <devopschandra42@gmail.com>
2025-11-17 21:27:33 +0530

    terraform

--- locals.tf
+++ locals.tf
@@ -4,6 +4,6 @@ locals {
         Terraform = true
         Project = "roboshop"
     }
-    az_names = data.aws_availability_zones.available.names
+    az_names = slicd(data.aws_availability_zones.available.names,0,2)
 }
 
